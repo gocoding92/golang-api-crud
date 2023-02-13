d249249b8b13a9b88ffcd1e4faaee0db091ded51
achmadrizky <rizky271092@gmail.com>
2023-02-13 17:41:42 +0700

    connect postgres sql

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5431
+	user     = "postgres"
+	password = "html5123"
+	dbname   = "kepegawaian"
+)
 
 func main() {
-	fmt.Println(123)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
 }
